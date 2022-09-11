3e448ce1c87a57c9f85710c36e8ec9da0d318999
jonas <jonas.voisard@gmail.com>
2022-09-11 20:08:14 +0200

    use node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 
+import node from '@astrojs/node'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
+        $src: '/src',
+        $types: '/src/types',
         $lib: '/src/lib',
         $layouts: '/src/layouts',
         $pages: '/src/pages',
@@ -14,4 +18,6 @@ export default defineConfig({
     },
   },
   integrations: [tailwind(), solidJs()],
+  output: 'server',
+  adapter: node(),
 })
