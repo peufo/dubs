3f374ea4714087574c5100c96880d3925acfdb13
jonas <jonas.voisard@gmail.com>
2022-10-14 19:00:47 +0200

    fix bad merge

--- src/lib/resources/Gallery.tsx
+++ src/lib/resources/Gallery.tsx
@@ -1,5 +1,5 @@
 import { For } from 'solid-js'
-
+import type { DirectusFile } from '$src/types'
 import { getAssetUrl } from '$src/directus'
 import './Gallery.css'
 import { Indicator } from '$lib/hexagon/Indicator'
@@ -10,7 +10,7 @@ interface Props {
 
 export function Gallery({ files }: Props) {
   // TODO: Traiter d'autre fichier ? PDF, text, etc...
-  const images = files // .filter((file) => file.type.startsWith('image'))
+  const images = files.filter((file) => file.type.startsWith('image'))
 
   return (
     <>
@@ -21,25 +21,18 @@ export function Gallery({ files }: Props) {
               <img
                 id={image.title}
                 class='rounded-xl'
-                src={getAssetUrl('image.id', 'height240')}
-                alt={''}
+                src={getAssetUrl(image.id, 'height240')}
+                alt={image.title}
               />
               <figcaption class='max-w-fit text-center m-auto my-2 text-gray-600 leading-none'>
-                decription
+                {image.description}
               </figcaption>
             </figure>
           )}
         </For>
       </div>
-      <div class='absolute top-8 h-60 left-0 w-6 from-transparent to-white bg-gradient-to-l grid items-center'>
-        <Icon
-          path={mdiChevronDoubleLeft}
-          size={28}
-          class='fill-white transition-transform m-4'
-          classList={{
-            'scale-0': scrollState() === 'none' || scrollState() === 'left',
-          }}
-        />
+      <div class='pb-6'>
+        <Indicator ids={images.map((img) => img.title)} />
       </div>
     </>
   )
