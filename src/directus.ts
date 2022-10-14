d4a17b291afebb996c7c9cdfd88de4a176f4bd16
jonas <jonas.voisard@gmail.com>
2022-10-14 18:42:10 +0200

    tray implement payload

--- src/directus.ts
+++ src/directus.ts
@@ -1,11 +1,10 @@
-import { Directus } from '@directus/sdk'
 import type { Collections } from '$types/index'
 
 export const directusUrl = 'http://localhost:8055'
-export const directus = new Directus<Collections>(directusUrl)
 
 type Preset = 'square240' | 'height240'
 
+/** @deprecated */
 export function getAssetUrl(id: string, preset: Preset) {
-  return `${directusUrl}/assets/${id}?key=${preset}`
+  return `${'TODO'}/assets/${id}?key=${preset}`
 }
