c832c263454f9879abeccb82e13a21c287c30c88
jonas <jonas.voisard@gmail.com>
2022-11-15 18:23:35 +0100

    handle title

--- site/src/lib/utils/serializeSlate.ts
+++ site/src/lib/utils/serializeSlate.ts
@@ -10,7 +10,14 @@ export function serialize(nodes: CustomTypes[]): string {
       if (node.italic) klass.push('italic')
       if (node.underline) klass.push('underline')
       if (node.strikethrough) klass.push('line-through')
+      if (node.type === 'h1') klass.push('text-5xl')
+      if (node.type === 'h2') klass.push('text-4xl')
+      if (node.type === 'h3') klass.push('text-3xl')
+      if (node.type === 'h4') klass.push('text-2xl')
+      if (node.type === 'h5') klass.push('text-xl')
+      if (node.type === 'h6') klass.push('text-lg')
       if (node.type === 'indent') klass.push('indent-2')
+      if (node.type === 'code') klass.push('font-mono')
 
       if (Text.isText(node))
         return `<span class="${klass}">${escapeHtml(node.text)}</span>`
