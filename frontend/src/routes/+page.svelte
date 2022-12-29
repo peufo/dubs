1a01b85fd777ccfacc80d4270b1aef44edcda4ce
jonas <jonas.voisard@gmail.com>
2022-12-29 19:15:11 +0100

    Add title and description

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+  <title>Dubs apiculture</title>
+  <meta
+    name="description"
+    value="La famille Dubey vous propose du miel de production artisanal"
+  />
+</svelte:head>
+
 <p>C'est un peu vide ici nan ?</p>
--- frontend/src/routes/edit/+page.svelte
+++ frontend/src/routes/edit/+page.svelte
@@ -2,4 +2,9 @@
   import Edit from '$lib/organism/Edit.svelte'
 </script>
 
+<svelte:head>
+  <title>Dubs EDIT</title>
+  <meta name="description" value="EDIT" />
+</svelte:head>
+
 <Edit />
