b5ae680f5e3dd12325c98e6eec5eab1644305fda
jonas <jonas.voisard@gmail.com>
2023-01-26 21:49:56 +0100

    remove trailling /

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         DUBS_PORT: 5004,
-        DUBS_ORIGIN: 'https://dubs-apiculture.ch/',
+        DUBS_ORIGIN: 'https://dubs-apiculture.ch',
         PAYLOAD_CONFIG_PATH: './build/backend/src/payload.config.js',
         MONGODB_URL: 'mongodb://localhost/dubs',
       },
