72a016a5135fa524311f78d7bb5c3076b9a9d078
jonas <jonas.voisard@gmail.com>
2023-02-03 05:37:58 +0100

    add height and width

--- frontend/src/lib/material/Watermark.svelte
+++ frontend/src/lib/material/Watermark.svelte
@@ -1,5 +1,7 @@
 <img
   src="/logo-borderless.png"
   alt="Logo dubs-apiculture"
+  width="880"
+  height="880"
   class="fixed top-1/2 left-0 rotate-12 max-h-[75%] -translate-y-1/2 -translate-x-1/2 opacity-10 -z-10"
 />
