358147451f5d3d068da21a032be3768dac55c5f8
jonas <jonas.voisard@gmail.com>
2023-02-03 03:23:05 +0100

    Pertinent show name

--- frontend/src/lib/Relations.svelte
+++ frontend/src/lib/Relations.svelte
@@ -5,6 +5,9 @@
   /** Chose the direction to develop next actions */
   export let type: 'input' | 'output'
   export let elements: HTMLElement[]
+
+  const showName = ({ name, action }: Relation) =>
+    !!name && (type === 'output' || !action)
 </script>
 
 <div
@@ -13,17 +16,17 @@
     {type === 'input' ? '-translate-y-1/2' : 'translate-y-1/2'}
   "
 >
-  {#each relations as relation, index}
+  {#each relations as relation, index (relation.id)}
     <div
       title={relation.name}
       class="border bg-white border-primary-dark rounded-full text-xs"
-      class:w-2={type === 'input' || !relation.name}
-      class:h-2={type === 'input' || !relation.name}
-      class:px-1={type === 'output' && !!relation.name}
+      class:w-2={!showName(relation)}
+      class:h-2={!showName(relation)}
+      class:px-1={showName(relation)}
       bind:this={elements[index]}
       data-relation-id={relation.id}
     >
-      {#if type === 'output' && !!relation.name}
+      {#if showName(relation)}
         {relation.name}
       {/if}
     </div>
